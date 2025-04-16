12b9e3b3a1def0faaab4c20f2df60b0161484159
Joe Mitchard <joemitchard92@gmail.com>
2025-04-16 07:58:52 +0100

    allow healing on wait

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -26,6 +28,9 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+    
+        let mut did_something = false;
+
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -36,6 +41,7 @@ pub fn player_input(
                 .for_each(|(entity, _)| {
                     // found a target, attack
                     hit_something = true;
+                    did_something = true;
                     commands.push(((), WantsToAttack{
                         attacker: player_entity,
                         target: *entity
@@ -43,11 +49,22 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push(((), WantsToMove{
                     entity: player_entity,
                     destination
                 }));
             }
+        }
+
+        if !did_something {
+            if let Ok(health) = ecs.entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1)
+            }
+            
         }
        
         *turn_state = TurnState::PlayerTurn;
